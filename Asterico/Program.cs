7d161dbe0bf6eb9ac0bd46993f7555f5038553a7
Eduardo Felipe <dudmont1@gmail.com>
2020-10-19 10:09:08 -0300

    exercicio do asterisco

---
+++ Asterico/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Asterico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Asterisco");
+
+            for(int linha = 0; linha < 10; linha++)
+            {
+                for (int Coluna = 0; Coluna <= linha; Coluna++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+          
+        }
+    }
+}
